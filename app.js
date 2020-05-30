3e93e627a58c18c450caf5f86d9e8a6ecbcfc61a
root <root@localhost.localdomain>
2020-05-30 19:31:24 +0530

    deploy 2

--- app.js
+++ app.js
@@ -14,7 +14,9 @@ console.log('Connected to Database');
   console.log('Connection Failed');
 });
 
+app.use("/", express.static(path.join(__dirname, "build")))
 app.use("/images", express.static(path.join(__dirname, "images")));
+
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader(
@@ -27,9 +29,9 @@ app.use((req, res, next) => {
     );
     next();
   });
-  app.use(express.static(path.join(__dirname, 'build')));
+
   app.use("/" , routes);
-  app.get('/', function(req, res) {
+  app.get(function(req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
   app.listen(9000);
